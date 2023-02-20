fceb8143e6e75d83e88006084a9ac2bad6654d99
Borisovna <hipsavita@gmail.com>
2023-02-20 21:38:24 +0300

    task 1 added trye

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int number)
+{
+if (number>99)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Тут нет третьей цифры :( Попробуй еще разок, дружок ;)");
+    return false;
+}
+    
+
+int trDigit(int num)
+{
+while (num >999)
+{
+    num/=10;
+}
+return num % 10;
+
+}
+ 
+ int number = Prompt ("Введите число"); 
+ if (Validate(number))
+ {
+    System.Console.WriteLine($"Третья цифра числа {number} это {trDigit(number)}");
+ }
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if (num <6 )
+    {
+        return true;
+    }
+    System.Console.WriteLine("Будни, дорогой, пошли работать:)>");
+    return false;
+}
+
+   System.Console.WriteLine($" {num} УРА!Выходной!Спим до 10");
+
+
+   //    if (num <>6 || num <>7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Будни, дорогой, пошли работать:)>");
+    return false;
+}
+
+   System.Console.WriteLine($" {num} УРА!Выходной!Спим до 10");
+
+
+
